36ab48a35631af99bff927939fcc4c371b83cd1e
Buds <mrbouyou@gmail.com>
2018-11-19 01:33:12 +0100

    timer:ScheduleTimerFixed use distinct args instead of a table condition type=unit : set default to player and fix display of empty line

--- WeakAurasOptions/ConditionOptions.lua
+++ WeakAurasOptions/ConditionOptions.lua
@@ -1151,7 +1151,7 @@ local function addControlsForIfLine(args, order, data, conditionVariable, condit
             order = order,
             values = currentConditionTemplate.values,
             get = function()
-              return currentConditionTemplate.values[check.value] and check.value or "member"
+              return not check.value and "player" or currentConditionTemplate.values[check.value] and check.value or "member"
             end,
             set = setValue
           }
@@ -1168,7 +1168,7 @@ local function addControlsForIfLine(args, order, data, conditionVariable, condit
             end,
             set = setValue,
             hidden = function()
-              return currentConditionTemplate.values[conditions[i].check.value] and conditions[i].check.value ~= "member"
+              return not conditions[i].check.value or currentConditionTemplate.values[conditions[i].check.value] and conditions[i].check.value ~= "member"
             end
           }
           order = order + 1;
