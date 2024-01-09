a0b45e08db00e11450463604445081620a2def9c
Infus <infus@squorn.de>
2024-01-10 00:26:55 +0100

    *** WIP

---
+++ .github/scripts/atlas_update.lua
@@ -0,0 +1,44 @@
+
+local function splitLine(line)
+  local result = {}
+  for col in line:gmatch('[^,%s]+') do
+    tinsert(result, col)
+  end
+  return unpack(result)
+end
+
+local validAtlasIds = {}
+local validAtlasMemberIds = {}
+local validNames = {}
+local invalidNames = {}
+
+for line in io.lines("UiTextureAtlasMember.csv") do
+  local atlasId = splitLine(line)
+  validAtlasIds[atlasId] = true
+  print("Found atlas Id", atlasId, " in ", line)
+end
+
+for line in io.lines("UiTextureAtlasMember.csv") do
+  local name, id, atlasId = splitLine(line)
+  if validAtlasIds[atlasId] and not validAtlasMemberIds[id] then
+    validAtlasMemberIds[id] = true
+    print("Found atlas member id", id, " in ", line)
+  end
+end
+
+for line in io.lines("UiTextureAtlasElement.csv") do
+  local name, id = splitLine(line)
+  if validAtlasMemberIds[id] then
+    validNames[name] = true
+  else
+    invalidNames[name] = true
+  end
+end
+
+for _, name in pairs(validNames) do
+  print("Found name", name)
+end
+
+for _, name in pairs(invalidNames) do
+  print("Ignoring name", name)
+end
---
+++ .github/scripts/wagotools-atlas.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for version in wow wow_classic wow_classic_era
+do
+  wget -O "UiTextureAtlas.csv" "https://wago.tools/db2/UiTextureAtlas/csv?${version}"
+  wget -O "UiTextureAtlasMember.csv" "https://wago.tools/db2/UiTextureAtlasMember/csv?${version}"
+  wget -O "UiTextureAtlasElement.csv" "https://wago.tools/db2/UiTextureAtlasElement/csv?${version}"
+
+  lua ./atlas_update.lua ${version}
+  if [ $? -ne 0 ]; then
+    echo "error while creating ${version} lua file"
+  else
+    echo "Success"
+    # mv ModelPaths*.lua ../../WeakAurasModelPaths/
+  fi
+done
