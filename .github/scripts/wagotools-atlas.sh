1f253f89dc1fb53485ec2fbb93ed64dd71dc75d7
Infus <infus@squorn.de>
2024-01-10 23:51:27 +0100

    **

--- .github/scripts/wagotools-atlas.sh
+++ .github/scripts/wagotools-atlas.sh
@@ -2,9 +2,9 @@
 
 for version in wow wow_classic wow_classic_era
 do
-  wget -O "UiTextureAtlas.csv" "https://wago.tools/db2/UiTextureAtlas/csv?${version}"
-  wget -O "UiTextureAtlasMember.csv" "https://wago.tools/db2/UiTextureAtlasMember/csv?${version}"
-  wget -O "UiTextureAtlasElement.csv" "https://wago.tools/db2/UiTextureAtlasElement/csv?${version}"
+  wget -O "UiTextureAtlas.csv" "https://wago.tools/db2/UiTextureAtlas/csv?branch=${version}"
+  wget -O "UiTextureAtlasMember.csv" "https://wago.tools/db2/UiTextureAtlasMember/csv?branch=${version}"
+  wget -O "UiTextureAtlasElement.csv" "https://wago.tools/db2/UiTextureAtlasElement/csv?branch=${version}"
 
   lua ./atlas_update.lua ${version}
   if [ $? -ne 0 ]; then
