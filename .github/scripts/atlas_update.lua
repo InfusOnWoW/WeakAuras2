b5a5ca3e30bf684932936acf4351f858faa0fc2d
Infus <infus@squorn.de>
2024-01-21 20:34:56 +0100

    Skip C_Guy

--- .github/scripts/atlas_update.lua
+++ .github/scripts/atlas_update.lua
@@ -10,33 +10,24 @@ end
 local validAtlasIds = {}
 local validAtlasElementIds = {}
 local validNames = {}
-local invalidNames = {}
-
-local oldNames = {}
 
 for line in io.lines("UiTextureAtlas.csv") do
   local atlasId = splitLine(line)
   validAtlasIds[atlasId] = true
-  --print("Found atlas Id", atlasId, " in ", line)
 end
 
 for line in io.lines("UiTextureAtlasMember.csv") do
   local name, id, uiTextureAtlasID, left, right, top, bottom, uiTextureAtlasElementId = splitLine(line)
-  name = name:lower()
   if validAtlasIds[uiTextureAtlasID] and not validAtlasElementIds[uiTextureAtlasElementId] then
     validAtlasElementIds[uiTextureAtlasElementId] = true
-    --print("Found atlas member id", id, " in ", line)
   end
-  oldNames[name] = true
 end
 
 for line in io.lines("UiTextureAtlasElement.csv") do
   local name, id = splitLine(line)
   name = name:lower()
-  if validAtlasElementIds[id] then
+  if validAtlasElementIds[id] and name:lower():sub(1, 5) ~= "cguy_" then
     validNames[name] = true
-  else
-    invalidNames[name] = true
   end
 end
 
