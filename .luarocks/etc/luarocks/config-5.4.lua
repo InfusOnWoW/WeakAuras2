e0dbdb8478bed0351d74ef6f4128c272f796720f
InfusOnWoW <InfusOnWoW@users.noreply.github.com>
2024-01-22 23:39:55 +0000

    Update atlas file list from wago.tools

---
+++ .luarocks/etc/luarocks/config-5.4.lua
@@ -0,0 +1,11 @@
+-- LuaRocks configuration
+
+rocks_trees = {
+   { name = "user", root = home .. "/.luarocks" };
+   { name = "system", root = "/home/runner/work/WeakAuras2/WeakAuras2/.luarocks" };
+}
+lua_interpreter = "lua";
+variables = {
+   LUA_DIR = "/home/runner/work/WeakAuras2/WeakAuras2/.lua";
+   LUA_BINDIR = "/home/runner/work/WeakAuras2/WeakAuras2/.lua/bin";
+}
